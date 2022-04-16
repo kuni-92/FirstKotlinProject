cc6dbad9da4855be38f21a2efe17eea9cb0060ba
kunihiro-dev <49485046+kunihiro-dev@users.noreply.github.com>
2022-04-16 21:40:44 +0900

    Add function to be named greet

--- src/main/kotlin/hello.kt
+++ src/main/kotlin/hello.kt
@@ -2,8 +2,13 @@ fun main() {
     println("Hello, World")
     val result = sum(1, 2)
     println(result)
+    println(greet("Tom"))
 }
 
 fun sum(a: Int, b:Int) :Int {
     return a + b
+}
+
+fun greet(name: String) :String {
+    return "Hello, $name"
 }
