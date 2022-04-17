786eb3b411184d3012fb477f48518a200d29f080
kunihiro-dev <49485046+kunihiro-dev@users.noreply.github.com>
2022-04-17 21:35:39 +0900

    The sum function refactored

--- src/main/kotlin/hello.kt
+++ src/main/kotlin/hello.kt
@@ -5,9 +5,7 @@ fun main() {
     println(greet("Tom"))
 }
 
-fun sum(a: Int, b:Int) :Int {
-    return a + b
-}
+fun sum(a: Int, b:Int) = a + b
 
 fun greet(name: String) :String {
     return "Hello, $name"
